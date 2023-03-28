00b97c509c0e4485bbcf1f27a92e81dd42a9fb3d
Roy <ekeneroy@gmail.com>
2023-03-27 20:49:25 -0500

    edge case breed not found update

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,14 +2,30 @@ const request = require("request");
 
 // const fs = require('fs')
 
-//const pathToSave = process.argv[3]
+const pathToSave = process.argv[2]
 const fetchBreedDescription = function (breedName, callback) {
   request(
     `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
     (error, response, body) => {
-      callback(error, response, body);
+
+      if (error) {
+        return callback(error, null);
+      }
+      const data = JSON.parse(body);
+      // console.log(data);
+      //console.log(typeof data);
+      if (!data.length) {
+        return console.log("Breed not found.")
+      }
+       else {
+        const description = data[0].description;
+        console.log(description);
+         
+      }
+      
     }
   );
 };
 
+fetchBreedDescription(pathToSave)
 module.exports = { fetchBreedDescription };
