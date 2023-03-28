8dbcea0d381150535a3e7d5d3e1aa8be890747b0
Roy <ekeneroy@gmail.com>
2023-03-27 21:13:36 -0500

    edge case request failed

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,30 +2,26 @@ const request = require("request");
 
 // const fs = require('fs')
 
-const pathToSave = process.argv[2]
+const pathToSave = process.argv[2];
 const fetchBreedDescription = function (breedName, callback) {
   request(
     `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
     (error, response, body) => {
-
       if (error) {
-        return callback(error, null);
+        console.log(`Error making request: ${error}`);
+      } else {
+        const data = JSON.parse(body);
+
+        if (!data.length) {
+          return console.log("Breed not found.");
+        } else {
+          const description = data[0].description;
+          console.log(description);
+        }
       }
-      const data = JSON.parse(body);
-      // console.log(data);
-      //console.log(typeof data);
-      if (!data.length) {
-        return console.log("Breed not found.")
-      }
-       else {
-        const description = data[0].description;
-        console.log(description);
-         
-      }
-      
     }
   );
 };
 
-fetchBreedDescription(pathToSave)
+fetchBreedDescription(pathToSave);
 module.exports = { fetchBreedDescription };
